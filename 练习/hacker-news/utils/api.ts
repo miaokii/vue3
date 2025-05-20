c42d78dcf3b764f36906b5be64f47031de137afb
miaokii <miaokii@sina.cn>
2025-05-20 23:51:28 +0800

    nav

---
+++ 练习/hacker-news/utils/api.ts
@@ -0,0 +1,20 @@
+export const feedsInfo = {
+    news: {
+        title: 'News', page: 10
+    },
+    newest: {
+        title: 'Newest', page: 12
+    },
+    ask: {
+        title: 'Ask', page: 2
+    },
+    show: {
+        title: 'Show', page: 2
+    },
+    jobs: {
+        title: 'Jobs', page: 1
+    },    
+}
+
+/// 获取所有有效的 feeds
+// export const validFeeds = Object.keys(feedsInfo)
