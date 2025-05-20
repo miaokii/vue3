e8909dc494e78bbf0e0673e571165c3b7d8e4037
康凯 <miaokii@sina.cn>
2025-05-20 18:00:32 +0800

    nuxt初始化

---
+++ 练习/hacker-news/app.vue
@@ -0,0 +1,12 @@
+<!-- git@github.com:nuxt/hackernews.git -->
+<template>
+    <app-header/>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ 练习/hacker-news/app/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="app-header">
+        Hello World
+    </header>
+</template>
+
+<script setup lang="ts" name="AppHeader">
+
+</script>
+
+<style scoped>
+
+.app-header {
+    height: 100px;
+    background: goldenrod;
+}
+
+</style>
